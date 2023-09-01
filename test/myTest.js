1beeeeaece028aad33b36a4f16712f6440d46e1f
Faheem Haider <faheemfaizan55@yahoo.com>
2023-09-01 11:55:02 +0500

    added test script

---
+++ test/myTest.js
@@ -0,0 +1,8 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("Sample Test Suite", () => {
+  it("should pass this test", () => {
+    expect(true).to.be.true; // This test will always pass
+  });
+});
